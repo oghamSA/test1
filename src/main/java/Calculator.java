8b2f4465c3d9cc483599a83b891a37b5d08862f9
oghamSA <o.dellerba@studenti.unisa.it>
2023-09-26 17:42:22 +0200

    Upload calculator

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int subtrack(int a, int b){
+        return a-b
+    }
+
+    public please(){
+        System.out.println("Please God make it work");
+    }
+
+
+}
